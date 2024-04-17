8bd2825ee8e6739a12ae11bfb4711728839d00aa
Rob Martin <rmartin3@alumni.unca.edu>
2024-04-16 22:31:41 -0400

    two sums

---
+++ problems/hash_map/1_two_sum.py
@@ -0,0 +1,30 @@
+'''
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+'''
+from typing import List
+from problems.run_tests import run_tests
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_map = {}  # To store value:index pairs
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
+        return -1
+    
+
+if __name__ == "__main__":
+    test_cases = [
+        ([2, 7, 11, 15], 9, [0, 1]),
+        ([3, 2, 4], 6, [1, 2]),
+        ([3, 3], 6, [0, 1]),
+    ]
+
+    s = Solution()
+    run_tests(test_cases, s.twoSum)
